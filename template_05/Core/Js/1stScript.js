941d27e396f4f01aa71e99cc0110ca8084c24495
riadghub <119328612+riadghub@users.noreply.github.com>
2022-12-12 12:23:59 +0100

    template 5 + animation portfolio

---
+++ template_05/Core/Js/1stScript.js
@@ -0,0 +1,16 @@
+let chiffreUn = 7;
+let chiffreDeux = 5;
+
+let prenom = "Riad";
+
+let age = 22;
+
+let sommeUnEtDeux = 7+5;
+
+const personne = [prenom, age];
+
+let username = prompt("Quel est votre nom ?");
+
+alert("Bienvenue "+username+" !");
+
+console.log(personne);
