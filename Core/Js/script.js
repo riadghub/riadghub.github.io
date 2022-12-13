3176a013bf9d65f17fa91a82eaa76f3e44c74064
riadghub <119328612+riadghub@users.noreply.github.com>
2022-12-13 12:22:27 +0100

    misc

---
+++ Core/Js/script.js
@@ -0,0 +1,7 @@
+const cursorStanding = document.querySelector(body);
+const cursorMoving = document.querySelector(body);
+
+const moveCursor = (e) => {
+    const mouseY = e.clientY;
+    const mouseX = e.clientX;
+}
