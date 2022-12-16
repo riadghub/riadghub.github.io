621351c427a04844511fe5f53a8abd2b0d13c223
riadghub <119328612+riadghub@users.noreply.github.com>
2022-12-16 16:56:38 +0100

    slides and temp5 improve

--- template_05/Core/Js/1stScript.js
+++ template_05/Core/Js/1stScript.js
@@ -1,15 +1,28 @@
-// let chiffreUn = 7;
-// let chiffreDeux = 5;
+let slideIndex = 1;
+showSlides(slideIndex);
 
-// let prenom = "Riad";
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-// let age = 22;
-// let sommeUnEtDeux = 7+5;
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-// const personne = [prenom, age];
-
-// let username = prompt("Quel est votre nom ?");
-
-// alert("Bienvenue "+username+" !");
-
-// console.log(personne);
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
