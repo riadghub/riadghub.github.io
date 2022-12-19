375ca5d80f29dc36bdf14fa22322c8ca043228be
riadghub <119328612+riadghub@users.noreply.github.com>
2022-12-19 11:45:51 +0100

    Update 1stScript.js

--- template_05/Core/Js/1stScript.js
+++ template_05/Core/Js/1stScript.js
@@ -2,12 +2,12 @@ let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
-function plusSlides(n) {
+function plusSlides(n){
   showSlides(slideIndex += n);
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
+function currentSlide(n){
   showSlides(slideIndex = n);
 }
 
@@ -15,12 +15,16 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  if (n > slides.length){
+    slideIndex = 1;
+  }
+  if (n < 1){
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++){
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
+  for (i = 0; i < dots.length; i++){
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
