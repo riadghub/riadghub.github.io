e8700652c51b2db4d6bd9128cfa7a2cfcf5e1d2a
riadghub <119328612+riadghub@users.noreply.github.com>
2023-01-03 17:25:08 +0100

    slideshow

--- Core/Js/script.js
+++ Core/Js/script.js
@@ -1,8 +1,27 @@
-// const cursor = document.querySelector('.cursor');
+// creer un window.onload
+window.onload = function(){
+	var mesLiensA = document.querySelectorAll('a');
+	var bigImage = document.querySelector('#big img');
+	var bigImageCaption = document.querySelector('figcaption');
+    // faire une boucle sur tous les liens
+	var attributImageL;
+	var attributImageT;
+	for(var i = 0; i < mesLiensA.length; i++){
+        // evenement click sur chaque lien, lancement de la fonction
+		mesLiensA[i].addEventListener('click',function(){
+			event.preventDefault();
+			attributImageL = this.getAttribute('href');
+			attributImageT = this.getAttribute('title');
+			bigImage.setAttribute('src',attributImageL);
+			bigImage.setAttribute('alt',attributImageT);
+			bigImageCaption.innerText = attributImageT;
+			for(var j = 0; j < mesLiensA.length; j++){
+				mesLiensA[j].style.borderColor = 'black';
+			}
+			this.style.borderColor = 'red';
+		});
+	}
 
-// document.addEventListener('mousemove', e => {
-//     cursor.setAttribute("style","top: "+e.pageY+"px ; left: "+e.pageX+"px;")
-// })
-
+};
 
 
