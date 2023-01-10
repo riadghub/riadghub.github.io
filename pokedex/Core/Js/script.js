4312ef0525ee09c4c5c4c5977048681ec5b5e276
riadghub <119328612+riadghub@users.noreply.github.com>
2023-01-10 11:04:36 +0100

    script api pokedex

--- pokedex/Core/Js/script.js
+++ pokedex/Core/Js/script.js
@@ -1,15 +1,29 @@
-fetch("https://pokebuildapi.fr/api/v1/pokemon");
+fetch('https://pokebuildapi.fr/api/v1/pokemon')
+  .then((response) => {
+    return response.json();
+})
+.then((data) => {
+  let myPokemons = data;
+  myButton.addEventListener('click',function () {
+    for(let i = 0; i < myPokemons.length; i++){
+      if(myText == myPokemons[i].name){
+        myName.innerHTML = myPokemons[i].name;
+        myImage.style.backgroundImage = "url("+myPokemons[i].image+")";
+      }
+      if(myText.value == myPokemons[i].id){
+        myName.innerHTML = myPokemons[i].name;
+        myImage.style.backgroundImage = "url("+myPokemons[i].image+")";
+      }
+    }
+  });
+});
 
 let myText = document.querySelector('input');
 let myButton = document.querySelector('button');
 let myName = document.querySelector('#name-screen');
 let myImage = document.querySelector('#main-screen');
 
-myButton.addEventListener('click',function () {
-    myImage.style.backgroundImage = ''; // récupérer valeur du myText pour récupérer image via API
-    myName.innerHTML = myText.value;
-  });
 
-console.log(myButton);
-console.log(myText);
-console.log(myName);
+// console.log(myButton);
+// console.log(myText);
+// console.log(myName);
