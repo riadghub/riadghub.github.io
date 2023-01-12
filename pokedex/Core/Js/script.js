a5f9977494bce5003669154d5d78fe4b1f497044
riadghub <119328612+riadghub@users.noreply.github.com>
2023-01-12 12:37:14 +0100

    pokedex minus and plus buttons

--- pokedex/Core/Js/script.js
+++ pokedex/Core/Js/script.js
@@ -7,13 +7,13 @@ let myText = document.querySelector('input');
 let myButton = document.querySelector('button');
 let myName = document.querySelector('#name-screen');
 let myImage = document.querySelector('#main-screen');
-let index;
 
 fetch('https://pokebuildapi.fr/api/v1/pokemon')
   .then((response) => {
     return response.json();
 })
 .then((data) => {
+  let index = 0;
   let myPokemons = data;
   myButton.addEventListener('click',function () {
     let myTextC = myText.value;
@@ -23,7 +23,6 @@ fetch('https://pokebuildapi.fr/api/v1/pokemon')
     }
     for(let i = 0; i < myPokemons.length; i++){
       if(myTextC === myPokemons[i].name){
-        index = i;
         myName.innerHTML = myPokemons[i].name;
         myImage.style.backgroundImage = "url("+myPokemons[i].image+")";
         myInformations.innerHTML = 'PV: '+myPokemons[i].stats.HP+'<br>Attaque: '+myPokemons[i].stats.attack+'<br>Défense: '+myPokemons[i].stats.defense+'<br>Attaque spéciale: '+myPokemons[i].stats.special_attack+'<br>Défense Spéciale: '+myPokemons[i].stats.special_defense+'<br>Vitesse: '+myPokemons[i].stats.speed;
@@ -33,6 +32,7 @@ fetch('https://pokebuildapi.fr/api/v1/pokemon')
           myType.innerHTML = ''+myPokemons[i].apiTypes[0].name+'<br>'+myPokemons[i].apiTypes[1].name;
         }
         myID.innerHTML = '#'+myPokemons[i].pokedexId;
+        index = myPokemons[i].pokedexId;
       }
       if(myText.value == myPokemons[i].id){
         myName.innerHTML = myPokemons[i].name;
@@ -44,21 +44,42 @@ fetch('https://pokebuildapi.fr/api/v1/pokemon')
           myType.innerHTML = ''+myPokemons[i].apiTypes[0].name+'<br>'+myPokemons[i].apiTypes[1].name;
         }
         myID.innerHTML = '#'+myPokemons[i].pokedexId;
+        index = myPokemons[i].pokedexId;
       }
     }
   });
   myButtonPlus.addEventListener('click',function(){
-    myName.innerHTML = myPokemons[index+1].name;
-    myImage.style.backgroundImage = "url("+myPokemons[index+1].image+")";
-    myInformations.innerHTML = 'PV: '+myPokemons[index+1].stats.HP+'<br>Attaque: '+myPokemons[index+1].stats.attack+'<br>Défense: '+myPokemons[index+1].stats.defense+'<br>Attaque spéciale: '+myPokemons[index+1].stats.special_attack+'<br>Défense Spéciale: '+myPokemons[index+1].stats.special_defense+'<br>Vitesse: '+myPokemons[index+1].stats.speed;
-    if(myPokemons[index+1].apiTypes.length == 1){
-      myType.innerHTML = ''+myPokemons[index+1].apiTypes[0].name;
-    }else{
-      myType.innerHTML = ''+myPokemons[index+1].apiTypes[0].name+'<br>'+myPokemons[index+1].apiTypes[1].name;
+    for(let i = 0; i < myPokemons.length; i++){
+      if(index === 897 || index === 898){
+        index = -1;
+      }
+      myName.innerHTML = myPokemons[index+1].name;
+      myImage.style.backgroundImage = "url("+myPokemons[index+1].image+")";
+      myInformations.innerHTML = 'PV: '+myPokemons[index+1].stats.HP+'<br>Attaque: '+myPokemons[index+1].stats.attack+'<br>Défense: '+myPokemons[index+1].stats.defense+'<br>Attaque spéciale: '+myPokemons[index+1].stats.special_attack+'<br>Défense Spéciale: '+myPokemons[index+1].stats.special_defense+'<br>Vitesse: '+myPokemons[index+1].stats.speed;
+      if(myPokemons[index+1].apiTypes.length == 1){
+        myType.innerHTML = ''+myPokemons[index+1].apiTypes[0].name;
+      }else{
+        myType.innerHTML = ''+myPokemons[index+1].apiTypes[0].name+'<br>'+myPokemons[index+1].apiTypes[1].name;
+      }
+      myID.innerHTML = '#'+myPokemons[index+1].pokedexId;
     }
-    myID.innerHTML = '#'+myPokemons[index+1].pokedexId;
+    index++;
   });
   myButtonMinus.addEventListener('click',function(){
-    alert('hello');
+    for(let i = 0; i < myPokemons.length; i++){
+      if(index === 0){
+        index = 898;
+      }
+      myName.innerHTML = myPokemons[(index-1)].name;
+      myImage.style.backgroundImage = "url("+myPokemons[(index-1)].image+")";
+      myInformations.innerHTML = 'PV: '+myPokemons[(index-1)].stats.HP+'<br>Attaque: '+myPokemons[(index-1)].stats.attack+'<br>Défense: '+myPokemons[(index-1)].stats.defense+'<br>Attaque spéciale: '+myPokemons[(index-1)].stats.special_attack+'<br>Défense Spéciale: '+myPokemons[(index-1)].stats.special_defense+'<br>Vitesse: '+myPokemons[(index-1)].stats.speed;
+      if(myPokemons[(index-1)].apiTypes.length == 1){
+        myType.innerHTML = ''+myPokemons[(index-1)].apiTypes[0].name;
+      }else{
+        myType.innerHTML = ''+myPokemons[(index-1)].apiTypes[0].name+'<br>'+myPokemons[(index-1)].apiTypes[1].name;
+      }
+      myID.innerHTML = '#'+myPokemons[(index-1)].pokedexId;
+    }
+    index--;
   });
 });
